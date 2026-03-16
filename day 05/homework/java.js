37adc2580988f1e7fb4efd737fd62ed5422c3aec
RealBatmane <nikalursmanadze@gmail.com>
2026-03-16 11:31:39 -0700

    GOA BEST

---
+++ day 05/homework/java.js
@@ -0,0 +1,33 @@
+// 1) დარეგრისტრირდი Codecademy-ზე და გაიარე მოცემული თავები.
+
+
+// 2) რა არის console.log()  დეტალურად ახსენი.
+ console log log aris erterti funqcia consoleis romelsac igive funqciaaq rogorc prins ubralod chven es console shi gamogvaq anu web shi
+
+// 3) ამ სტრინგის მეთოდებზე მოიყვანე თითო-თოთო მაგალითი და კოდი ახსენი კომენტარის სახით:
+// .charAt() - mititebul indexze myof monacebs abrunebs
+// .concat() - imistvis ro 2 an 2 ze meti sityvaze string gaaertianos
+// .slice()  - es aris sliceing sadac vutitebt starts with pirvels da meores endswirth
+// .toUpperCase() -  gaxdis yvela asos upper case
+// .toLowerCase() gaxdis yvela asos lower case
+// .trim() - acilebs spaceebs orive mxridan
+// .trimStart()  - acliebs space mxolod marcxena mxares 
+// .trimEnd() - es kide marjvena mxares 
+// .repeat()  - gaameorebs imdenjer ramdenjerac mivutitet brchxilebshi
+// .replace() -  rac brchxilebshi weria ipovis texstshi da pirvelive rac iqneba msgavsi rac brchxilebshi mivutitet mxolod magas washlis
+// .replaceAll()  - rac brchxilebshi weria ipovis texstshi da rac iqneba msgavsi rac brchxilebshi mivutitet mxolod magas washlis
+// .split()  -  gadaaqcevs listad da tu mivutitet space brchxilebshi yovel space ze gayofs texts magalitad hello world hello da world gaiyofa da ava listshi
+// .startsWith() - amowmebs iwyeba tu ara amit
+// .endsWith() - amowmebs mtavrdeba tu ara amit
+
+// 4) მოიძიე რა არის Math ობიექტი? ეს მეთოდები ახსენი კომენტარის სახით და მოიყვანე თითო-თითო მაგალითი:
+// Math.round() - mititebul ricxvs abrunebs damrgvalebulad
+// Math.ceil() - tu ricxvia 3.10 abrunebs 4 anu adis zevit 
+// Math.floor() - tu ricxvi aris 3.10 abrunebs 3 anu qvevit chadsi
+// Math.trunc()  atwiladian ricxvidan mxolod mtels abrunebs 4.19 -- -- -- 4
+// Math.sign() - gveubnbea ricxvi aris tu ara dadebiti uaryofiti an udris tu ara nuls
+// Math.pow() - jer vutitebt rixvs shemdeg ki raze unda gavamravlot 
+// Math.sqrt() - abrunebs ricxvis fesvs
+// Math.min() - abrunebs yvelaze mcire ricxvs
+// aseve gvaqvs math.abs(x) - romelic abrunebs raodenobas ramdenic aklia mititebul ricxvs nulamde -3.10 shemtxvevashi ---- 3.10 uklia 
+// aseve gvaqvs max romelic abrunebs maximum anu yvelaze did ricxvs
